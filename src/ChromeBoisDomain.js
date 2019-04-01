3122390f9f01abe65056f53686065b2790cf24bc
Manny Pissanos <mpissano@gmail.com>
2019-04-01 20:32:57 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -11,6 +11,8 @@ export default class ChromeBoisDomain extends Component {
   handleKeyDown = (event) => {
     if (event.key === 'a') {
       resize('+')
+      else if(event.key === 's')
+      resize('-')
     }
   }
    
