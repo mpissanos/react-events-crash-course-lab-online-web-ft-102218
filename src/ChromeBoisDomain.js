e6b01abb67609241feecb155105f433a686c05c3
Manny Pissanos <mpissano@gmail.com>
2019-04-01 20:26:48 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -24,6 +24,7 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={this.toggleCycling}
         width='900'
         height='600'
         tabIndex="0">
